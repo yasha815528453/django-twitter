b7addcbb58982d994c45d576c6a9377d8de29d59
yasha815528453 <yasha8155@gmail.com>
2022-11-16 17:09:01 -0800

    create tweet model and api

--- twitter/settings.py
+++ twitter/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
     'debug_toolbar',
+    'tweets',
 ]
 
 REST_FRAMEWORK = {
--- twitter/urls.py
+++ twitter/urls.py
@@ -17,16 +17,19 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from accounts.api.views import UserViewSet, AccountViewSet
+from tweets.api.views import TweetViewSet
 
 import debug_toolbar
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename="accounts")
+router.register(r'api/tweets', TweetViewSet, basename='tweets')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('__debug__/', include(debug_toolbar.urls)),
+
 ]
