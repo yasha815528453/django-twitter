f78d294c6056dabc88ea66a8a3f6dca10fa39ec0
yasha815528453 <yasha8155@gmail.com>
2022-11-17 00:17:03 -0800

    minor fix

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -5,13 +5,13 @@ from rest_framework.exceptions import ValidationError
 
 
 
-class FollowerSerializer(serializers.ModelSerializer):
-    user = UserSerializer(source='from_user')
-    created_at = serializers.DateTimeField()
+class FriendshipSerializerForCreate(serializers.ModelSerializer):
+    from_user_id = serializers.IntegerField()
+    to_user_id = serializers.IntegerField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('from_user_id', 'to_user_id')
 
     def validate(self, attrs):
         if attrs['from_user_id'] == attrs['to_user_id']:
@@ -19,6 +19,14 @@ class FollowerSerializer(serializers.ModelSerializer):
                 'message': 'from_user_id and to_user_id should be different'
             })
         return attrs
+
+    def create(self, validated_data):
+        from_user_id = validated_data['from_user_id']
+        to_user_id = validated_data['to_user_id']
+        return Friendship.objects.create(
+            from_user_id=from_user_id,
+            to_user_id=to_user_id,
+        )
 
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializer(source='from_user')
